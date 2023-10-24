67ecc424af5fbceb477e9cb9e23ef1195d6acea7
unknown <armore_16@hotmail.com>
2023-10-24 17:46:08 -0500

    added questions for the readME into index.js and installed npm

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please name your Project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe the purpose and functionality of this project.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select a license applicable to this project.",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    },
+    {
+        type: "input",
+        name: "require",
+        message: "List any project dependencies here.", 
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "State the languages or technologies associated with this project.",
+    },
+    {
+        type: "input",
+        name: "creator",
+        message: "Write your GitHub username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Provide valid email address.",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list any contributors (Use GitHub username).",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide walkthrough of required tests if applicable.",
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
