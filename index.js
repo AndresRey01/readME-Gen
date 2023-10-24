5b77397bc441e360146095aeef3379fc23d63d91
unknown <armore_16@hotmail.com>
2023-10-24 18:12:10 -0500

    index.js close to done just fixing bugs on where the readme will be created

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Including packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
+// Creating an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -53,17 +53,17 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
+// Writing README.md File
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Initializing app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log("Creating Professional README.md File...");
-        writeToFile()
-    })
+        writeToFile("/readme-gen/README.md", generateMarkdown({ ...responses }));
+    });
 }
 
 // Function call to initialize app
